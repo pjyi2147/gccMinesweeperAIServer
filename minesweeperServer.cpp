225c79d0437bd09103cce57017bfbb16456e8b66
Jeongseop Yi <yjs990427@gmail.com>
2017-08-17 20:30:37 -0400

    optimization

--- minesweeperServer.cpp
+++ minesweeperServer.cpp
@@ -12,90 +12,95 @@
 using namespace std;
 using json = nlohmann::json;
 
-void GameLoop(int order, MineSweeper* m)
+void printHeader(int level)
+{
+	switch (level)
+	{
+	case 1:
+		cout << endl << "Beginner Mode" << endl << endl;
+		break;
+	case 2:
+		cout << endl << endl << "Intermediate Mode" << endl << endl;
+		break;
+	case 3:
+		cout << endl << endl << "Expert Mode" << endl << endl;
+		break;
+	case 4:
+		cout << endl << endl << "Custom Mode" << endl << endl;
+	}
+}
+
+void GameLoop(int level, MineSweeper* m)
 {
 	while (!m->returnGameEnd())
 	{
 		string command;
-		json orders;
 		int col, row;
 		// clear console
 		system("clear"); 
 		// mode print
-		switch (order)
-		{
-		case 1:
-			cout << endl << "Beginner Mode" << endl << endl;
-			break;
-		case 2:
-			cout << endl << endl << "Intermediate Mode" << endl << endl;
-			break;
-		case 3:
-			cout << endl << endl << "Expert Mode" << endl << endl;
-			break;
-		case 4:
-			cout << endl << endl << "Custom Mode" << endl << endl;
-		}
+		printHeader(level);
 		// print minefield
 		m->printMineField();
 
 		// check win
 		m->checkWin();
 		// if game ended somehow break
-		if (m->returnGameEnd()) {
-			ingameTransfer(&orders, m);
+		if (m->returnGameEnd()) 
+		{
+			ingameTransfer(m);
 			break;
 		}
 		// read command
 		if (!m->returnGameEnd() &&
 			!m->returnWin())
-			ingameTransfer(&orders, m);
+			ingameTransfer(m);
 	}
 }
 
 int main()
 {
-	while (true) {
-		int col, row, mineNum, order;
+	while (true) 
+	{
+		int col, row, mineNum, level;
 		string command;
 		string orders;
 
-		vector<string> k;
-		k.push_back("any string");
 		cout << "Level 1: Beginner (9x9, 10)" << endl;
 		cout << "Level 2: Intermediate (16x16, 40)" << endl;
 		cout << "Level 3: Expert (30x16, 10)" << endl;
 		cout << "Level 4: Custom" << endl;
-		startGamefunction(order);
+		startGamefunction(level);
 
-		switch (order)
+		switch (level)
 		{
-		case 1:
-			col = 9, row = 9, mineNum = 10;
-			cout << "Beginner Mode" << endl << endl;
-			break;
-		case 2:
-			col = 16, row = 16, mineNum = 40;
-			cout << "Intermediate Mode" << endl << endl;
-			break;
-		case 3:
-			col = 30, row = 16, mineNum = 99;
-			cout << "Expert Mode" << endl << endl;
-			break;
-		case 4:
-			cout << "Custom Mode" << endl << endl;
-			cout << "Input col: ";
-			cin >> col;
-			cout << "Input row: ";
-			cin >> row;
-			cout << "Input mineNum: ";
-			cin >> mineNum;
-			if (mineNum > col * row - 1) {
-				cout << "Error: mineNum too big" << endl;
+			case 1:
+				col = 9, row = 9, mineNum = 10;
+				cout << "Beginner Mode" << endl << endl;
+				break;
+			case 2:
+				col = 16, row = 16, mineNum = 40;
+				cout << "Intermediate Mode" << endl << endl;
+				break;
+			case 3:
+				col = 30, row = 16, mineNum = 99;
+				cout << "Expert Mode" << endl << endl;
+				break;
+			case 4:
+				cout << "Custom Mode" << endl << endl;
+				cout << "Input col: ";
+				cin >> col;
+				cout << "Input row: ";
+				cin >> row;
 				cout << "Input mineNum: ";
-
 				cin >> mineNum;
-			}
+				if (mineNum > col * row - 1) 
+				{
+					cout << "Error: mineNum too big" << endl;
+					cout << "Input mineNum: ";
+
+					cin >> mineNum;
+				}
 		}
 
 		auto minesweeper = MineSweeper(col, row, mineNum);
@@ -109,7 +114,7 @@ int main()
 		// transfer first script;
 		firstScriptTransfer(col, row, command, &minesweeper);
 		// loop until the game ends
-		GameLoop(order, &minesweeper);
+		GameLoop(level, &minesweeper);
 
 		minesweeper.EndGame(minesweeper.returnWin());
 	}
